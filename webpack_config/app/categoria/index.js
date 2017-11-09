17b01e8beb1d02be422af64c970b2c8d213c115b
Carlos Herrera <jcarloshm_93@yahoo.com>
2017-11-09 00:18:29 -0600

    Mostrando Datos del menu

--- webpack_config/app/categoria/index.js
+++ webpack_config/app/categoria/index.js
@@ -4,7 +4,8 @@ import config from './config'
 if (!firebase.apps.length) {
 	firebase.initializeApp(config)
 }
-
+var index = ''
+var htmlcate = ''
 var db = firebase.database()
 const categoria = () => {
 	var html = ''
@@ -16,13 +17,44 @@ const categoria = () => {
 			var key = keys[i]
 			var equipo = arrayDatos[key]
 			'<h1>${equipo.Modelo}</h1>'
+			
+			htmlcate = `
+				  <table>
+				    <thead>
+				      <tr>
+				        <th>Equipo Moviles</th>
+				      </tr>
+				    </thead>
+				  </table>
+			`
+			html += htmlcate	
 		}
+
+		index = `
+		<main class="main-content">
+			<div class="container">
+				<div class="page">
+					<section>
+						<header>
+							<h2 class="section-title">Menu categoria</h2>
+						</header>
+						<div class="product-list">
+							${html}
+						</div>
+					</section>
+				</div> 
+			</div>
+		</main> 
+		`
+		const main = document.querySelector('main')
+		main.innerHTML = index
 
 		console.log(keys)
 		
 	}
 
 	db.ref('Equipos Moviles').once('value').then(obtenerDatos)
+
 
 	/*const main = document.querySelector('main')
 	main.innerHTML = layout(categoria)*/
