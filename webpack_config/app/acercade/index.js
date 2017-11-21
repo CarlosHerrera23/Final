29d9a0a62044879d4ef47225b33a8d4a4c253079
Carlos Herrera <jcarloshm_93@yahoo.com>
2017-11-21 02:13:08 -0600

    Modifique el inicio puro css

--- webpack_config/app/acercade/index.js
+++ webpack_config/app/acercade/index.js
@@ -7,8 +7,10 @@ const acercade = `
         <li>
           <section class="slider__section">
             <div class="slider__contenido">
-          <h2 class="slider__contenido__titulo">Quienes Somos</h2>
-               <p class="slider__contenido__texto">Una Compañia Que Se Dedica A la Venta De Equipo de Celulares</p>
+          <h2 class="slider__contenido__titulo">! ! Quienes Somos ! !</h2>
+               <p class="slider__contenido__texto">Una Compañia Que Se Dedica A la Venta De Equipo de Celulares que se origino en el 2017</p>
+               <p class="slider__contenido__texto">La Tienda de Telefonos esta creada para aquellas personas que se les hace dificl ir a tiendas a compar dispositivos moviles</p>
+               <p class="slider__contenido__texto">TechMarts Le facilicita al cliente eso ya que cuenta con servicio de entrega hasta la puerta del hogar</p>
           </div>
           <img src="https://cdn.wallpapersafari.com/1/98/dZ5tsj.jpg" class="slider__img">
           </section>
@@ -19,7 +21,8 @@ const acercade = `
           <section class="slider__section">
             <div class="slider__contenido">
           <h2 class="slider__contenido__titulo">La Tienda</h2>
-          <p class="slider__contenido__texto">Las Ventas Son Solo En la Web</p>
+            <p class="slider__contenido__texto">Las Ventas Son Solo En la Web ya que por el momento no contamos con una sucurlas pero las compras en linea son 100 % seguras</p>
+            <p class="slider__contenido__texto">Si su equipo cuenta con algun problema la tienda TechMarts se hace responsable por dicha falla </p>
           </div>
             <img src="https://cdn.wallpapersafari.com/41/29/lvbMFB.jpg" class="slider__img">
             </section>
@@ -29,8 +32,10 @@ const acercade = `
         <li>
           <section class="slider__section">
             <div class="slider__contenido">
-              <h2 class="slider__contenido__titulo">Que Equipos</h2>
+              <h2 class="slider__contenido__titulo">! !Que Equipos ! !</h2>
               <p class="slider__contenido__texto">Se Vende Todo Tipo De Equipos</p>
+              <p class="slider__contenido__texto">Desde Smartphone del año 2015 hasta los mas actuales de las mejores marcas y mejores precios</p>
+              <p class="slider__contenido__texto">Marcas Como SAMSUNG , LG , IPHONE , MOTOROLA , SONY , ETC....
             </div>
             <img src="https://wallpaperscraft.com/image/apple_vs_android_android_competition_26151_1920x1080.jpg" class="slider__img">
             </section>
@@ -41,7 +46,9 @@ const acercade = `
           <section class="slider__section">
             <div class="slider__contenido">
               <h2 class="slider__contenido__titulo">Se Parte De Nuestros Clientes</h2>
-              <p class="slider__contenido__texto">La Tienda Le Ofrece Grandes Procmociones A Sus Clientes</p>
+              <p class="slider__contenido__texto">La Tienda Le Ofrece Grandes Procmociones A Sus Clientes y descuentos a nuestros fieles cliente</p>
+              <p class="slider__contenido__texto">Convierte en nuestro cliente con tal solo comprar un equipo o un accesorio</p>
+              <p class="slider__contenido__texto">Esperamos Que Nuestra web sea de su agrado</p>
             </div>
             <img src="http://wallup.net/wp-content/uploads/2016/01/140848-3D-anaglyph_3D-cityscape-triangle.jpg" class="slider__img">
             </section>
