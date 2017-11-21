d5d964ce45045e5e5faa4cd3c19cf6676736f478
Carlos Herrera <jcarloshm_93@yahoo.com>
2017-11-21 03:43:23 -0600

    agrege el footer

--- webpack_config/app/login.js
+++ webpack_config/app/login.js
@@ -18,6 +18,9 @@ body{
       font-family: monospace;
       padding: 10em;
   }
+  #googleLogin{
+  	cursor:pointer;
+  }
 
 </style>
 <br>
@@ -34,6 +37,29 @@ body{
          			<p align="center">
          		        <input type="button" id="botonatras" value="<-------  Atras <-------" name="Atrás" onclick="history.back()" /></p>
                 </form>
+
+                <br><br><br><br><br>
+                <footer>
+      
+			      <div class="sociales">
+			      <p id="letra" class="copy">TechMarts &copy; 2017</p>
+			        <a href="https://www.facebook.com/">
+			        <img id="uno" name="facebook" src="https://image.flaticon.com/icons/svg/185/185981.svg"></a>
+			        <a href="https://twitter.com/">
+			        <img id="uno" name="twiter" src="https://image.flaticon.com/icons/svg/185/185961.svg"></a>
+			        <a href="https://www.instagram.com/?hl=es-la">
+			        <img id="uno" name="instagram" src="https://image.flaticon.com/icons/svg/174/174855.svg"></a>
+			        <a href="https://github.com/CarlosHerrera23">
+			        <img id="uno" name="github" src="https://image.flaticon.com/icons/svg/25/25471.svg"></a>
+			        <a href="https://www.youtube.com/">
+			        <img id="uno" name="youtube" src="https://image.flaticon.com/icons/svg/185/185983.svg"></a>
+			        <a href="https://plus.google.com/">
+			        <img id="uno" name="gplus" src="https://image.flaticon.com/icons/svg/185/185963.svg"></a>
+			        <a href="https://www.twitch.tv/">
+			        <img id="uno" name="twitch" src="https://image.flaticon.com/icons/svg/167/167304.svg"></a>
+			      </div>
+			    </div>
+			  </footer>
 
          	`
 
