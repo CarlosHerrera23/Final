dcfaf7cf65fd46d7f6ae08be16b86d501cd6eeb0
Carlos Herrera <jcarloshm_93@yahoo.com>
2017-11-26 03:38:25 -0600

    Se movio aqui la edicion de producto

--- webpack_config/app/edicion/template.js
+++ webpack_config/app/edicion/template.js
@@ -24,59 +24,10 @@ const templateDetalle = (equipo) => {
               </div>
             </div>
           </div>
+            
 
-          <div class="col s12 m6">  
-         <div class="row">
-    <form class="col s12">
-    <div class="card tarjeta" style="padding-bottom:10px">
-     <div class="card-content letra" >
-     <span class="card-title">Editar Productos</span>
-      <div class="row">
-        <div class="input-field col s6">
-          <input id="Phone" type="text" class="validate">
-          <label for="Phone">Equipo</label>
-        </div>
-        <div class="input-field col s6">
-          <input id="Camara" type="text" class="validate">
-          <label for="Camara">Camara</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s6">
-          <input id="Capacidad" type="text" class="validate">
-          <label for="Capacidad">Capacidad</label>
-        </div>        
-        <div class="input-field col s6">
-          <input id="OS" type="text" class="validate">
-          <label for="OS">OS</label>
-        </div>
-    
-      <!--<div class="row">
-        <div class="col s12">
-          <div class="file-field input-field">
-            <div class="btn">
-              <span>Selecionar Imagen</span>
-              <input type="file" id="file">
-            </div>
-            <div class="file-path-wrapper">
-              <input class="file-path validate" type="text">
-            </div>
           </div>
-        </div>
-      </div>  
-      </div>--> 
-  </div>   
-    </form>
-  </div>
-  </div>
-  <div id="ver123"></div>
-        </div>        
-      </div>
-  <div class="row">
-    <div class="col s12" id="botones">
-     <a class="waves-effect waves-light btn"  id="actualizar" class ="button" key="${equipo.id}" img="${equipo.URL}">Modificar</a>
-
-        </div>
+         
       </div>
 
 
