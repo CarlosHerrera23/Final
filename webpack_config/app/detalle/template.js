323a4bb983652aa80b49d6130e867af221986910
Carlos Herrera <jcarloshm_93@yahoo.com>
2017-11-26 03:37:47 -0600

    Se modifico el template de update

--- webpack_config/app/detalle/template.js
+++ webpack_config/app/detalle/template.js
@@ -49,7 +49,9 @@ const template = (equipo) => {
               }
               <p><strong>Camara:</strong> ${equipo.Camara}</p>
               <p><strong>Capacidad:</strong> ${equipo.Capacidad}</p>
+              <p><strong>Color:</strong> ${equipo.Color}</p>
               <p><strong>OS:</strong> ${equipo.OS}</p>
+
 
                <div>
               <br>
@@ -60,7 +62,64 @@ const template = (equipo) => {
               </div>
             </div>
           </div>
+          <br><br><br>
+           <div class="col s12 m6">  
+         <div class="row">
+  <form class="col s12">
+    <div class="card tarjeta" style="padding-bottom:10px">
+     <div class="card-content letra" >
+     <span class="card-title">Editar Productos</span>
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="Phone" type="text" class="validate">
+          <label for="Phone">Equipo</label>
         </div>
+        <div class="input-field col s6">
+          <input id="Camara" type="text" class="validate">
+          <label for="Camara">Camara</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="Capacidad" type="text" class="validate">
+          <label for="Capacidad">Capacidad</label>
+        </div>        
+        <div class="input-field col s6">
+          <input id="OS" type="text" class="validate">
+          <label for="OS">OS</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="Color" type="text" class="validate">
+          <label for="Color">Color</label>
+        </div>
+      <div class="row">
+        <div class="col s12">
+          <div class="file-field input-field">
+            <div class="btn">
+              <span>Selecionar Imagen</span>
+              <input type="file" id="file">
+            </div>
+            <div class="file-path-wrapper">
+              <input class="file-path validate" type="text">
+            </div>
+          </div>
+        </div>
+      </div>  
+      </div>
+  </div>   
+    </form>
+  </div>
+  </div>
+  <div id="ver123"></div>
+        </div>        
+      </div>
+  <div class="row">
+    <div class="col s12" id="botones">
+     <a class="waves-effect waves-light btn" style="cursor:pointer"  id="actualizar" class ="button" key="${equipo.id}" img="${equipo.URL}">Modificar</a>
+
+       
+        </div>
+
       </div>
 
       
